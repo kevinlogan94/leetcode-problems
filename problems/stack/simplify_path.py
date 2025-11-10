8c872a608a136e24b84992a02a91ed8bfe8a12b3
kevinlogan94 <kevinmlogan7@gmail.com>
2025-11-10 13:21:54 -0500

    simplify_path + variant

--- problems/stack/simplify_path.py
+++ problems/stack/simplify_path.py
@@ -1,3 +1,5 @@
+# https://www.youtube.com/watch?v=AFVN1As_tqc
+
 # You are given an absolute path for a Unix-style file system, which always begins with a slash '/'. Your task is to transform this absolute path into its simplified canonical path.
 
 # The rules of a Unix-style file system are as follows:
@@ -6,6 +8,7 @@
 # A double period '..' represents the previous/parent directory.
 # Multiple consecutive slashes such as '//' and '///' are treated as a single slash '/'.
 # Any sequence of periods that does not match the rules above should be treated as a valid directory or file name. For example, '...' and '....' are valid directory or file names.
+
 # The simplified canonical path should follow these rules:
 
 # The path must start with a single slash '/'.
@@ -14,74 +17,48 @@
 # The path must not have any single or double periods ('.' and '..') used to denote current or parent directories.
 # Return the simplified canonical path.
 
+#  -------------------------------------
 
+# parameters - path: string
+# return - simplified_path: string
 
-
-
-# Parameter - absolutepath: str
-# return - simplifiedpath: str
-
-# /test/folder/one
-
-# Q - /test/folder///one -> test/folder/one
-# Q - /test/folder/../one -> test/one
-# Q - /test/..../one -> /test/..../one
-# Q - 
-
-# 1. if . -> remove it -> /test/./one -> /test/one
-# 2. if .. -> move up -> /test/../one -> /one
-# 3. if //+ -> replace with / -> /test///one -> /test/one.    with split, this becomes check ""
-
-# split("/")
-# stack
-
-# /test/folder/../one
-# run split
-# splitlist = ["test", "folder", "..", "one"]
-# stack = []
-
-# loop through this
-# stack = ["test", "one"]
-# join(stack) -> /test/one
-
-# /test/folder/../one/./two////three -> /test/one/two/three
-# run split
-# splitlist = ["test", "folder", "..", "one", ".", "two", "", "", "", "three"]
-# stack = ["test", "one", "two", "three"]
-
-
-# Time - O(n) -> n = len(splitlist)
-# Space - O(n) -> n = len(splitlist)
-
-
-# 1. def class
-# 2  def method
-# 3. declare variables
-# 4. run split into splitlist
-# 5. loop through splitlist
-# 6.     if "." or ".."
-# 7.     elif ".." pop stack
-# 8.     else append to stack
-# 9.  return the stack joined   
-
-
+# /asdf///./as//.. -> /asdf/as/.. -> /asdf
+# /. -> /
+# /// -> /
+# // -> /
+# /../ -> /
+# /... -> /...
 # 
-class Solution:
-    def simplify_path(self, path: str) -> str:
-        stack = []
-        splitlist = path.split("/")
 
-        for item in splitlist:
-            if item == "." or item == "":
+# 1 split("/")
+# 2 loop through split_list and build result list based on the values in split_list
+# 3  if . -> do nothing
+# 4  if .. -> pop from our result list
+# 5  if /// or // or ///+ -> "" -> do nothing
+# 6  if 
+# 7 return the joined list with / delimiter with a prefix /
+
+# Time - O(n) -> n = len(split_list)
+# Space - O(n) -> n = len(split_list)
+
+class solution:
+    def simplify_path(self, path: str) -> str:
+        split_list = path.split("/")
+        result_list = []
+
+        for item in split_list:
+            if item == ".":
                 continue
             elif item == "..":
-                if stack:
-                    stack.pop()
+                if result_list:
+                    result_list.pop()
+            elif item == "":
+                continue
             else:
-                stack.append(item)
+                result_list.append(item)
         
-        return "/" + "/".join(stack)
-    
+        return "/" + "/".join(result_list)
+
 
 
 # META Variant
@@ -98,3 +75,60 @@ class Solution:
 # Return:
 # The string representation of the absolute path you end up at after running cd from the given cwd.
 # Your result should be in simplified form.
+
+# parameters - cwd: str, cd: str
+# return - simplified_path: str
+
+# cwd -> /test
+# cd -> //..
+# res -> /
+
+# cwd -> /test/sample
+# cd -> /.
+# res -> /test/sample
+
+# cwd -> /test/sample
+# cd -> /test
+# res -> /test/sample/test
+
+# cwd -> /
+# cd -> /..
+# res -> /
+
+# cwd -> 
+# cd -> 
+# res ->
+
+# prep cwd -> split("/")
+# prep cd -> split("/")
+
+# loop through cd
+#   if . -> do nothing
+#   if "" -> do nothing
+#   if ".." -> pop cwd if cwd has values
+#   else -> append value to cwd
+
+
+# Time - O(n) -> n = len(split_cd)
+# Space - O(k) -> k = the len of splt_cd or split_cwd depending on which is greater
+
+class Solution:
+    def simplify_path(self, cd: str, cwd: str) -> str:
+        if not cd:
+            cd = "/"
+        if not cwd:
+            cwd = "/"
+
+        split_cd = cd.split("/")
+        split_cwd = cwd.split("/")
+
+        for item in split_cd:
+            if item == "." or item == "":
+                continue
+            elif item == "..":
+                if split_cwd:
+                    split_cwd.pop()
+            else:
+                split_cwd.append(item)
+        
+        return "/" + "/".join(split_cwd)
