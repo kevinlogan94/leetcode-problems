39120f0e6fb662afdc611aff32419c1582e2f7a3
kevinlogan94 <kevinmlogan7@gmail.com>
2025-11-19 15:57:05 -0500

    max swap and merge intervals

--- problems/greedy/max_swap.py
+++ problems/greedy/max_swap.py
@@ -5,35 +5,64 @@
 #  --------------
 
 
-# parameter - num: int
-# return - max_num: int
+# Parameters: num: int
+# return: max_num: int
 
-# Q - num is always a valid int
-# Q - num can't be negative
-# Q - leading 0 in num isn't going to be passed
+# Q - no negative nums
 
-# num = 1234
-# return = 4231
+# max_swap
+# two pointers
 
-# num = 9123
-# return = 9321
+# 12345 -> 52341
+# 45231 -> 54231
+# 95452 -> 95542
+# 99129
 
-# num = 918123
+# 
 
-# max_num - max num
-# max_i - index of the max num that we will potentially want to swap
+# swap
+# potential swap 
 
-# swap_g - greater value to swap
-# swap_l - lesser value to swap
+# swap_g - num[0] + 1 and greater
+# swap_l - num[end] - 1 and lesser
 
-#  update swap pointers when you find a number less than the current max_num
 
-# 1 define class
-# 2 define method
-# 3 convert num to list of strings
-# 4 loop backwards through list of strings
-# 5   check if max num
-# 6.  check if num less than max num
+# 
+# declare variables - swap_l, swap_r, p_swap_r, convert num to list[chars]
+# first num is greatest num - p_swap_r
+# loop through our nums right to left, start at len(num) - 1
+#   check if num is less than p_swap_r -> if so, set swap_l and swap_r
+#   check if num is greater than p_swap_r -> if so, update p_swap_r
+
+
+# Time - O(n) - n is the length of num
+# Space - O(n) - n is the length of num
+
+class Solution:
+    def max_swap(self, num: Optional[int]) -> int:
+        # The problem constraints usually guarantee num is not None, but this is safe.
+        if num is None:
+            return num
+        
+        nums = list(str(num))
+        
+        # Create a map to store the last seen index of each digit.
+        last_occurrence = {int(digit): i for i, digit in enumerate(nums)}
+
+        # Iterate from left to right to find the first digit that can be swapped.
+        for i, digit in enumerate(nums):
+            # Check for a larger digit (from 9 down to current digit + 1).
+            for d in range(9, int(digit), -1):
+                # If a larger digit exists and its last occurrence is after the current index...
+                if d in last_occurrence and last_occurrence[d] > i:
+                    # ...we've found our optimal swap.
+                    j = last_occurrence[d]
+                    nums[i], nums[j] = nums[j], nums[i]
+                    return int("".join(nums))
+
+        # If no swap was made, the number is already the largest possible.
+        return int("".join(nums))
+    
 
 
 class Solution:
@@ -60,6 +89,3 @@ class Solution:
         nums[index_l], nums[index_g] = nums[index_g], nums[index_l]
 
         return int("".join(nums))
-        
-
-
---
+++ problems/greedy/merge_intervals.py
@@ -0,0 +1,54 @@
+# Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and 
+# return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+# -------------------
+
+# parameters - intervals: Optional[List[Tuple(int, int)]]
+# return - new_intervals: Optional[List[Tuple(int, int)]]
+
+# [()]
+
+# Q - interval values are ints
+# Q - These are not sorted
+
+# [(0,1),(1,2),(0,2)]
+# [(0,1),(0,2),(1,2)]
+
+# [(0,3),(0,2)] -> [(0,3)]
+# [(0,3)]
+
+# sort our list by tuple[0]
+# declare variable - merge_intervals = []
+# loop through our tuples starting at list[i + 1]
+#    do comparison - 
+#       if prev last int is > than curr_first_int
+#           create new tuple that is (0, max(right_int))
+#           append merge_intervals
+#       else append i-1 to merge intervals
+
+# Time - O(nlogn) - n = len(intervals)
+# Space - O(n) - n = len(intervals)
+
+
+# [(0,1),(2,4)(3,4)]
+class Solution:
+    def merge_intervals(self, intervals: Optional[List[Tuple[int, int]]]) -> Optional[List[Tuple[int, int]]]:
+        if not intervals:
+            return intervals
+        
+        sorted_intervals = sorted(intervals, key=lambda i: i[0])
+        merged_intervals = [sorted_intervals[0]]
+
+        for interval in sorted_intervals[:1]:
+            last_merge_interval = merged_intervals[-1]
+            if last_merge_interval[1] > interval[0]:
+                right_spot = max(last_merge_interval[1], interval[1])
+                left_spot = last_merge_interval[0]
+                new_interval = (left_spot, right_spot)
+
+                merged_intervals[-1] = new_interval
+            else:
+                merged_intervals.append(interval)
+        
+        return merged_intervals
+                                                                                      
