23c0adfe430d01d993d09a7b31453eb14c9efef9
kevinlogan94 <kevinmlogan7@gmail.com>
2025-11-19 16:17:23 -0500

    do merge intervals

--- problems/greedy/merge_intervals.py
+++ problems/greedy/merge_intervals.py
@@ -51,4 +51,45 @@ class Solution:
                 merged_intervals.append(interval)
         
         return merged_intervals
-                                                                                      
+
+
+# Variant
+# Given two array of intervals A and B, where intervals in A have no overlap in A and intervals in B have no overlap in B. 
+# Furthermore, A[i], B[i] = [start_i, end_i], merge all overlapping intervals between the two interval lists, 
+# and return an array of the non-overlapping intervals that cover all the intervals in the input.Note: Both $A$ and $B$ 
+# are sorted by their start in ascending order.
+
+# -------------------
+
+# parameters - A: Optional[List[Tuple(int, int)]], B: Optional[List[Tuple(int, int)]]
+# return - new_intervals: Optional[List[Tuple(int, int)]]
+
+# 
+
+# A - [(1,2),(3,5)]
+# B - [(1,4),(2,5)]
+
+# merge them together
+
+class Solution:
+    def merge_two_interval_lists(self, A: Optional[List[Tuple[int, int]]], B: Optional[List[Tuple[int, int]]]) -> Optional[List[Tuple[int, int]]]:
+        intervals = A + B
+        if not intervals:
+            return []
+
+        sorted_intervals = sorted(intervals, key=lambda i : i[0])
+        merged_intervals = [sorted_intervals[0]]
+
+        for interval in sorted_intervals[1:]:
+            last_interval_merged = merged_intervals[-1]
+
+            # Check for overlap (or if intervals are touching)
+            if last_interval_merged[1] >= interval[0]:
+                # Merge by creating a new tuple
+                right = max(last_interval_merged[1], interval[1])
+                merged_intervals[-1] = (last_interval_merged[0], right)
+            else:
+                # No overlap, add the new interval
+                merged_intervals.append(interval)
+        
+        return merged_intervals
