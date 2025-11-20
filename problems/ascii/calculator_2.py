f3a07f16b13e3a92beb81155332689429a386f32
kevinlogan94 <kevinmlogan7@gmail.com>
2025-11-20 09:35:32 -0500

    basic calc 2

--- problems/ascii/calculator_2.py
+++ problems/ascii/calculator_2.py
@@ -8,6 +8,58 @@
 
 # --------------------------------------
 
+# parameters: s: str, 
+# return: value: float
+
+# "1 + 1"
+# "2+1*3"
+# "2+1-1*2/3"
+
+# Q - negative values valid
+# Q - +- */
+
+# array of operations []
+# loop through our string
+#  if digit - build out our num
+#  if operator - 
+#    switch -
+#      + - add the num to our operators array
+#      - - set the num as negative, then add the num to our operators array
+#      * - multiply the last value in our operator by the num
+#      / - divide the last value in our operator by the num - be mindful of dividing in python
+# return sum(operators)
+
+# Time - O(n) n = the length of s
+# Space - O(n) n = the length of s
+
+class Solution:
+    def calculator(self, s: str) -> float:
+        operators = []
+        num = 0
+        operation = "+"
+        s += "+" # Append a dummy operator to process the last number
+
+        for char in s:
+            if char.isspace():
+                continue
+
+            if char.isdigit():
+                num = num * 10 + int(char)
+            else:
+                match operation:
+                    case "+":
+                        operators.append(num)
+                    case "-":
+                        operators.append(-num)
+                    case "*":
+                        operators[-1] *= num
+                    case "/":
+                        # Handle truncation toward zero for negative numbers
+                        operators[-1] = int(operators[-1] / num)
+                num = 0
+                operation = char
+
+        return sum(operators)
 
 
 
@@ -15,3 +67,26 @@
 
 # Variant
 # Same problem but only + and *
+
+class Solution:
+    def calculator(self, s: str) -> int:
+        operators = []
+        num = 0
+        operation = "+"
+        s += "+"
+
+        for char in s:
+            if char == "":
+                continue
+            elif char.isdigit():
+                num = num * 10 + int(char)
+            else:
+                match operation:
+                    case "+":
+                        operators.append(num)
+                    case "*":
+                        operators[-1] *= num
+                num = 0
+                operation = char
+            
+        return sum(operators)
